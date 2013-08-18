b3cd529067ca72861705a5ca1476780e04d4a299
Varbanov <stefanvarbanoff@abv.bg>
2013-08-18 19:20:05 +0300

    Exceptions handling solutions added

---
+++ 013. Exceptions handling/01.CalcSqrt/CalcSqrt.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class CalcSqrt
+{
+    //01. Write a program that reads an integer number and calculates and prints its square root.
+    //If the number is invalid or negative, print "Invalid number". In all cases finally print
+    //"Good bye". Use try-catch-finally.
+
+    static double CalculateSqrt(int num)
+    {
+        if (num < 0)
+        {
+            throw new ArgumentException("The number must be positive!");
+        }
+        else
+        {
+            double res = Math.Sqrt((double)num);
+            return res;
+        }
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Please enter an integer: ");
+            CalculateSqrt(int.Parse(Console.ReadLine()));
+        }
+        catch (FormatException fe)
+        {
+            Console.Error.WriteLine("Invalid number! " + fe.Message);
+        }
+        catch (ArgumentException outOfRange)
+        {
+            Console.Error.WriteLine(outOfRange.Message);
+        }
+        catch (OverflowException overflowEx)
+        {
+            Console.Error.WriteLine("Error! The number must be smaller or equal to {0}! {1}", int.MaxValue, overflowEx);
+        }
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
+
---
+++ 013. Exceptions handling/04.DownloadFromInternet/DownloadFromInternet.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+class DownloadFromInternet
+{
+    //04. Write a program that downloads a file from Internet (e.g. http://www.devbg.org/img/Logo-BASD.jpg) 
+    //and stores it the current directory. Find in Google how to download files in C#. Be sure to catch 
+    //all exceptions and to free any used resources in the finally block.
+
+ private static string ExtractNameFromURL(string url)
+    {
+        int startIndexOfName = url.LastIndexOf('/') + 1;
+        StringBuilder fileName = new StringBuilder();
+        for (int i = startIndexOfName; i < url.Length; i++)
+        {
+            fileName.Append(url[i]);
+        }
+        return fileName.ToString();
+    }
+
+    static void Main()
+    {
+        //input
+        Console.WriteLine("Please enter the full url path:");
+        string url = Console.ReadLine();
+        //string url = @"http://www.devbg.org/img/Logo-BASD.jpg";
+
+        //extract file name
+        string fileName = ExtractNameFromURL(url);
+
+        //solution
+        WebClient client = new WebClient();
+        try
+        {
+            //the file is downloaded in the current directory of the program (...\bin\debug)
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, client);
+            client.DownloadFile(url, fileName);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("The address parameter is null.");
+        }
+        catch (WebException webEx)
+        {
+            Console.WriteLine(webEx.Message);
+        }
+        catch (NotSupportedException notSupportedEX)
+        {
+            Console.WriteLine(notSupportedEX.Message);
+        }
+        finally
+        {
+            client.Dispose();
+        }
+
+        ////instead, one can use "using" block to dispose automatically:
+        //using (WebClient client = new WebClient())
+        //{
+        //    string fileName = "your_file.jpg";
+        //    Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, client);
+        //    client.DownloadFile(url, fileName);
+        //}
+    }
+
+   
+}
+

ae56110e71d48856bb4adf13dadea17b291c85c9
Varbanov <stefanvarbanoff@abv.bg>
2013-08-20 14:44:05 +0300

    some text files solutions added

---
+++ 014.TextFiles/01.PrintOddLinesOfFile/PrintOddLinesOfFile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class PrintOddLinesOfFile
+{
+    //Write a program that reads a text file and prints on the console its odd lines.
+
+    static void PrintOddLines(StreamReader reader)
+    {
+        using (reader)
+        {
+            string currLine = reader.ReadLine();
+            //counting from one
+            int counter = 1;
+            while (currLine != null)
+            {
+                if (counter % 2 == 0)
+                {
+                    Console.WriteLine(currLine);
+                }
+                currLine = reader.ReadLine();
+                counter++;
+            }
+        }
+    }
+
+    static void Main()
+    {
+        StreamReader reader = new StreamReader(@"..\..\PrintOddLinesOfFile.cs", Encoding.GetEncoding("Windows-1251"));
+        //Това е кирилица първи ред
+        //Това е кирилица втори ред
+        PrintOddLines(reader);
+    }
+}
+

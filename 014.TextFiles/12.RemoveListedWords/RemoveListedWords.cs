dd1650fce5254664aaca41f8dcc2bde69da9ad72
Varbanov <stefanvarbanoff@abv.bg>
2013-08-22 16:30:38 +0300

    text files solutions added

---
+++ 014.TextFiles/12.RemoveListedWords/RemoveListedWords.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class RemoveListedWords
+{
+    //12.Write a program that removes from a text file all words listed in given another text file. Handle all possible exceptions in your methods.
+
+    static void RemoveWords(string inputPath, string listedWordsPath)
+    {
+        List<string> list = ExtractList(listedWordsPath);
+        List<string> outputLines = new List<string>();
+        StreamReader reader = new StreamReader(inputPath);
+        using (reader)
+        {
+            while (reader.Peek() != -1)
+            {
+                string currLine = reader.ReadLine();
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    //match each word from the list in the current line and replace it with empty string if matched
+                    string currWord = "\\b" + list[i] + "\\b";
+                    currLine = Regex.Replace(currLine, currWord, "");
+                }
+                outputLines.Add(currLine);
+            }
+        }
+        //overwrite the file
+        StreamWriter writer = new StreamWriter(inputPath);
+        using (writer)
+        {
+            foreach (var line in outputLines)
+            {
+                writer.WriteLine(line);
+            }
+        }
+    }
+
+    private static List<string> ExtractList(string listedWordsPath)
+    {
+        //read listed words and return them in a list
+        StreamReader reader = new StreamReader(listedWordsPath);
+        List<string> list = new List<string>();
+        using (reader)
+        {
+            while (reader.Peek() != -1)
+            {
+                list.Add(reader.ReadLine().Trim());
+            }
+        }
+
+        return list;
+    }
+
+    static void Main()
+    {
+        try
+        {
+            string path = @"..\..\input.txt";
+            string listPath = @"..\..\list.txt";
+            RemoveWords(path, listPath);
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.Error.WriteLine("The directory of either the input file or the list file is missing");
+
+        }
+        catch (FileNotFoundException)
+        {
+            Console.Error.WriteLine("The input file or the list file is missing!");
+        }
+        catch (IOException)
+        {
+            Console.Error.WriteLine("An input/output error occured");
+        }
+    }
+}
+
---
+++ 014.TextFiles/13.CountSomeWordsInFile/CountSomeWordsInFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+    
+class CountSomeWordsInFile
+{
+    //Write a program that reads a list of words from a file words.txt and finds how many times 
+    //each of the words is contained in another file test.txt. The result should be written in 
+    //the file result.txt and the words should be sorted by the number of their occurrences in 
+    //descending order. Handle all possible exceptions in your methods.
+
+    static void Main()
+    {
+    }
+}
+

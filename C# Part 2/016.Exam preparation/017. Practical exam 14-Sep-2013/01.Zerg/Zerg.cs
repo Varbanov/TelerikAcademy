7bbd8b3108b167f3b64ccad6cd112c579bd09796
Varbanov <stefanvarbanoff@abv.bg>
2014-01-18 21:21:26 +0200

    CS part 2 exam solutions added

---
+++ C# Part 2/016.Exam preparation/017. Practical exam 14-Sep-2013/01.Zerg/Zerg.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Zerg
+{
+    static string[] PopulateNums()
+    {
+        string[] nums = new String[15];
+        nums[0] = "Rawr";
+        nums[1] = "Rrrr";
+        nums[2] = "Hsst";
+        nums[3] = "Ssst";
+        nums[4] = "Grrr";
+        nums[5] = "Rarr";
+        nums[6] = "Mrrr";
+        nums[7] = "Psst";
+        nums[8] = "Uaah";
+        nums[9] = "Uaha";
+        nums[10] = "Zzzz";
+        nums[11] = "Bauu";
+        nums[12] = "Djav";
+        nums[13] = "Myau";
+        nums[14] = "Gruh";
+
+        return nums;
+    }
+
+    static Stack<string> SeparateTokens(string input)
+    {
+        Stack<string> tokens = new Stack<string>();
+        int counter = 0;
+        StringBuilder tempToken = new StringBuilder();
+
+        while (counter < input.Length)
+        {
+            tempToken.Append(input[counter]);
+            counter++;
+
+            if (counter % 4 == 0)
+            {
+                tokens.Push(tempToken.ToString());
+                tempToken.Clear();
+            }
+        }
+        return tokens;
+    }
+
+    static long ConvertZergToDecimal(Stack<string> tokens, string[] nums)
+    {
+        long num = 0;
+
+        long mult = 15;
+        int power = 0;
+        for (int i = tokens.Count - 1; i >= 0; i--)
+        {
+            string currToken = tokens.Peek();
+            int decNum = Array.IndexOf(nums, tokens.Pop());
+            num += (long) Math.Pow(mult, power) * decNum;
+            power++;
+        }
+        return num;
+    }
+
+
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+
+        string[] nums = PopulateNums();
+
+        Stack<string> tokens = SeparateTokens(input);
+
+        long res = ConvertZergToDecimal(tokens, nums);
+
+        Console.WriteLine(res);
+    }
+}
+
---
+++ C# Part 2/017. Practical exam 14-Sep-2013/Exam 2013-2014 @ 14-Sep-2013 Morning/DecodeAndDecrypt/DecodeAndDecrypt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DecodeAndDecrypt
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int cypherLength = ExtractCypherLength(input);
+        string messageAndCypher = input.Substring(0, input.Length - cypherLength.ToString().Length);
+        string decoded = Decode(messageAndCypher);
+        string message = decoded.Substring(0, decoded.Length - cypherLength);
+        string cypher = decoded.Substring(message.Length);
+        string decrypted = Decrypt(message, cypher);
+        Console.WriteLine(decrypted);
+    }
+
+    static string Decrypt(string message, string cypher)
+    {
+        StringBuilder result = new StringBuilder();
+
+        if (cypher.Length < message.Length)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                int m = (int)message[i] - 65;
+                int c = (int)cypher[i % cypher.Length] - 65;
+                int xor = m ^ c;
+                result.Append((char)(xor + 65));
+            }
+        }
+        else
+        {
+            int excessivePassCount = cypher.Length - message.Length;
+            int position = 0;
+            for (int i = 0; i < message.Length; i++)
+            {
+                int m = (int)message[i] - 65;
+                int c = (int)cypher[i] - 65;
+                if (excessivePassCount > 0)
+                {
+                    int excesive = cypher[message.Length + position] - 65;
+                    int xor = (m ^ c) ^ excesive;
+                    position++;
+                    excessivePassCount--;
+                    result.Append((char)(xor + 65));
+                }
+                else
+                {
+                    int xor = m ^ c;
+                    result.Append((char)(xor + 65));
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private static int ExtractCypherLength(string input)
+    {
+        int counter = input.Length - 1;
+        while (char.IsDigit(input[counter]))
+        {
+            counter--;
+        }
+        int result = int.Parse(input.Substring(counter + 1));
+        return result;
+    }
+
+    private static string Decode(string input)
+    {
+        StringBuilder decoded = new StringBuilder();
+        int position = 0;
+        while (position < input.Length)
+        {
+            if (!char.IsDigit(input[position]))
+            {
+                decoded.Append(input[position]);
+                position++;
+            }
+            else
+            {
+                StringBuilder tempDigits = new StringBuilder();
+                while (position < input.Length && char.IsDigit(input[position]))
+                {
+                    tempDigits.Append(input[position]);
+                    position++;
+                }
+
+                int count = int.Parse(tempDigits.ToString());
+
+                if (count > 2)
+                {
+                    string str = new String(input[position], count);
+                    decoded.Append(str);
+                    position++;
+                }
+                else
+                {
+                    decoded.Append(tempDigits);
+                }
+            }
+        }
+        return decoded.ToString();
+    }
+}
+

2a3882285363f19e46012ed1f00923230c4f000e
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-05-02 21:11:06 +0300

    ie 8 issue

---
+++ problem ie 8/task_03.js
@@ -0,0 +1,25 @@
+﻿if (!document.querySelectorAll) {
+           document.querySelectorAll = function(selector) {
+               switch (selector[0]) {
+                   case '#': return document.getElementById(selector.substr(1));
+                       break;
+                   case '.': return document.getElementsByClassName(selector.substr(1));
+                       break;
+                   default: return document.getElementsByName(selector.substr(1));
+                       break;
+               }
+           }
+       }
+
+if (!document.querySelector) {
+    document.querySelector = function (selector) {
+        switch (selector[0]) {
+            case '#': return document.getElementById(selector.substr(1))[0];
+                break;
+            case '.': return document.getElementsByClassName(selector.substr(1))[0];
+                break;
+            default: return document.getElementsByName(selector.substr(1))[0];
+                break;
+        }
+    }
+}

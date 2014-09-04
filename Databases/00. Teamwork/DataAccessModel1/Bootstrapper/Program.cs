56123add843fffffd9ddc359df3ac17ba7673047
Stefan Varbanov <stefanvarbanoff@abv.bg>
2014-09-04 23:19:06 +0300

    Databases team work project included

---
+++ Databases/00. Teamwork/DataAccessModel1/Bootstrapper/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Telerik.OpenAccess;
+using DataAccessModel2;
+
+
+namespace QuickStartBootstrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UpdateDatabase();
+
+            using (var ctx = new FluentModel())
+            {
+                var pesho = new Stefan() { Name = "stefannnn" };
+                ctx.Add(pesho);
+                ctx.Delete(pesho);
+                ctx.SaveChanges();
+
+            }
+
+            Console.Write("Database update complete! Press any key to close.");
+            Console.ReadKey();
+        }
+        private static void UpdateDatabase()
+        {
+            using (var context = new DataAccessModel2.FluentModel())
+            {
+                var schemaHandler = context.GetSchemaHandler();
+                EnsureDB(schemaHandler);
+            }
+        }
+        private static void EnsureDB(ISchemaHandler schemaHandler)
+        {
+            string script = null;
+            if (schemaHandler.DatabaseExists())
+            {
+                script = schemaHandler.CreateUpdateDDLScript(null);
+            }
+            else
+            {
+                schemaHandler.CreateDatabase();
+                script = schemaHandler.CreateDDLScript();
+            }
+            if (!string.IsNullOrEmpty(script))
+            {
+                schemaHandler.ExecuteDDLScript(script);
+            }
+        }
+    }
+}
---
+++ Databases/00. Teamwork/DataAccessModel1/DataAccessModel2/Stefan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessModel2
+{
+    public class Stefan
+    {
+
+        public int StefanID { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
